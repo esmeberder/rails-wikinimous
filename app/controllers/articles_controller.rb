1d4446c1cda85cd4eea52bb98080a3802a09e116
Esme Berder <esmeyangli@gmail.com>
2019-07-23 23:28:44 +0800

    wiki

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,23 +9,35 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
   end
 
   def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to @article
   end
 
   def edit
   end
 
   def update
+    @article.update(article_params)
+    redirect_to @article
   end
 
   def delete
+    @article.destroy
+    redirect_to articles_path
   end
 
   private
 
   def set_article
     @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :content)
   end
 end
