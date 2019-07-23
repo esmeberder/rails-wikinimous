552ff5dcaa39ffd260f8a8642957ae463aa3a80a
Esme Berder <esmeyangli@gmail.com>
2019-07-23 22:47:39 +0800

    Rails new with frontend and form gems

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,31 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+end
